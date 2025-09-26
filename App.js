dc3edea603ae2288b41ffff96836c0723f80b32a
unknown <arthurruivo486@gmail.com>
2025-09-26 10:46:34 -0300

    Finaliza da prova do 2° info turma B

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>2o Ano B</Text>
+      </View>
+      <View>
+        <Text>2035</Text>
+      </View>
+      <View>Richard Rodrigues da Silva</View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
